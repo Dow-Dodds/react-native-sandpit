8d2e590a6d11047f609f7ca41c7b70ffe571b9e4
Dow-Dodds <100536374+Dow-Dodds@users.noreply.github.com>
2024-08-14 22:43:15 +1200

    Initial commit

--- App.tsx
+++ App.tsx
@@ -1,10 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, ScrollView, StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+
+// Define the type for Box props
+type BoxProps = {
+  color: string;
+  children?: React.ReactNode; // `children` can be any valid React node
+};
+
+// Define the Box component with typed props
+const Box: React.FC<BoxProps> = ({ color, children }) => (
+  <View style={[styles.box, { backgroundColor: color }]}>
+    {children}
+  </View>
+);
 
 export default function App() {
+  const [pressedCount, setPressedCount] = useState(0);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <View style={styles.textWrapper}>
+        <Text style={styles.text}>Hello World</Text>
+      </View>
+
+      <View style={{ height: 400 }}>
+        <ScrollView horizontal>
+          <Box color="orange">
+            <Text>My Name is Dow</Text>
+          </Box>
+          <Box color="blue">
+            <Text>This is my first React Native App</Text>
+          </Box>
+        </ScrollView>
+      </View>
+
+      <Text style={{ margin: 16 }}>
+        {pressedCount > 0
+          ? `The button was pressed ${pressedCount} times!`
+          : 'The button isn\'t pressed yet'
+        }
+      </Text>
+      <Button
+        title="Press Me"
+        onPress={() => setPressedCount(pressedCount + 1)}
+        disabled={pressedCount > 3}
+      />
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +55,30 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'cream',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  box: {
+    width: 400,
+    height: 400,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  textWrapper: {
+    backgroundColor: 'pink',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 100,
+    height: 100,
+  },
+  text: {
+    fontStyle: 'normal',
+    color: 'red',
+    padding: 4,
+    fontFamily: 'Roboto',
+    fontSize: 20,
+    textAlign: 'center',
+    fontWeight: 'bold',
   },
 });
